bd2c6b67f29ee89c0b6417cf4731c702e8753aae
Neil <nmm85@cornell.edu>
2020-04-26 22:54:53 -0400

    fixed html_helper in engine

--- engine.ml
+++ engine.ml
@@ -3,18 +3,21 @@ open Core
 open Mechaml
 open Printf
 open List
+open Stdlib
 
 let get_url (stock:string) = 
   "https://finance.yahoo.com/quote/AAPL/key-statistics/"
 
-let get_html_helper lst = 
-  ""
+let rec get_html_helper (lst:(string*string) list) = 
+  match lst with 
+  | [] -> ""
+  | (id,leaf)::t -> if (id = "Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)") then leaf else get_html_helper t
 
 let get_html stock = 
   Soup.parse (read_file "html/apple.html") 
   |> Soup.select "span" 
   |> Soup.to_list 
-  |> List.map ~f:(fun span -> Soup.R.id span, Soup.R.leaf_text span)
+  |> List.map (fun span -> Soup.R.id span, Soup.R.leaf_text span)
   |> get_html_helper
 
 let print_price stock = 
