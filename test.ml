2b2eaa9eb62deb3088cc8cea84b824f8ee29bc2b
Neil <nmm85@cornell.edu>
2020-05-09 00:48:27 -0400

    testing fix

--- test.ml
+++ test.ml
@@ -2,10 +2,10 @@ open OUnit
 open Engine
 
 let tests = [
+  ("test save" >:: fun _ -> assert_equal () (Engine.save_file "AAPL"));
   ("test url" >:: fun _ -> assert_equal "https://www.marketbeat.com/stocks/NASDAQ/AAPL/" (Engine.get_url "AAPL"));
   ("test file" >:: fun _ -> assert_equal "html/AAPL.html" (Engine.get_file "AAPL"));
   (* ("test body" >:: fun _ -> assert_equal "AAPL.html" (Engine.file "AAPL")); *)
-  ("test save" >:: fun _ -> assert_equal () (Engine.save_file "AAPL"));
   ("test price" >:: fun _ -> assert_equal 620.26 (Engine.get_price "AAPL" 2));
 
 ]
